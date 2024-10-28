a111d763a1a4558fd234006dcdfc82dad27e8b30
Gustavo <118356161+torneira@users.noreply.github.com>
2024-10-28 08:26:14 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -55,8 +55,34 @@ app.get("/usuarios",async(req,res)=>{
         await conexao.end()
         //PASSO 4: Colocar os dados do banco no response
         res.send(result)
-    }catch(e){
+    }catch(err){
         res.status(500).send("Erro do servidor")
+    }
+    
+    
+})
+
+app.post("/produtos",async(req,res)=>{
+
+    //O que eu tenho que fazer aqui dentro?
+    //OK -> PASSO 1: Criar o banco de dados
+    //PASSO 2: Usar a lib mysql2 para conectar com o banco
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const {id, nome, descricao, preco, imagem} = req.body
+        const [result,fields]  = await conexao.query("INSERT INTO produtos VALUES (?,?,?,?,?)", [id,nome,descricao,preco,imagem])
+        await conexao.end()
+        //PASSO 4: Colocar os dados do banco no response
+        res.send(result)
+    }catch(er){
+        res.status(500).send("Erro do servidor")
+        console.log(er)
     }
     
     
