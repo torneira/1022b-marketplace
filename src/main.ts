19eed905456b0e07eead3b1213cb16f92b437a25
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-10-28 07:53:58 -0400

    Código de post de produtos

--- src/main.ts
+++ src/main.ts
@@ -33,10 +33,31 @@ app.get("/produtos",async(req,res)=>{
         res.send(result)
     }catch(e){
         res.status(500).send("Erro do servidor")
-    }
-    
-    
+    }  
 })
+
+app.post("/produtos",async(req,res)=>{
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022b",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result,fields]  = 
+            await conexao.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
+                [id,nome,descricao,preco,imagem])
+        await conexao.end()
+        res.send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro do servidor")
+    }  
+})
+
+
 
 
 //INICIAR O SERVIDOR
