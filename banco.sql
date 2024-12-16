b64650de5937c9efd19641260be8c35e5a8c6f0d
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-12-16 07:12:21 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,4 +1,6 @@
-USE defaultdb;
+drop database banco1022b;
+create database banco1022b;
+USE banco1022b;
 CREATE TABLE IF NOT EXISTS produtos(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
@@ -6,4 +8,4 @@ CREATE TABLE IF NOT EXISTS produtos(
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
-INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
+INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'q=tbn:ANd9GcSJFIZtuDMrlGBOk07nhYjWUaxYXc31YEOK92qg9markFMrwK9tXltvy6J63eu01shSUNkdcwcQazAZcW3u8V8grKxsHAuoyv7KqmpFoiFj8WVyahmnS06wyXWJVv3er7ymJdT2YPc&usqp=CAc');
