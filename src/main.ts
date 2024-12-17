44252a83ae30f721075fd686662b99d0612c5b46
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-12-17 08:01:52 -0400

    Separando a conexão com um banco em uma classe.

--- src/main.ts
+++ src/main.ts
@@ -13,23 +13,14 @@ app.use(express.json())  //Middleware
 //incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
 app.use(cors())
 //ROTAS
-app.get("/produtos",async(req,res)=>{
 
-    //O que eu tenho que fazer aqui dentro?
-    //OK -> PASSO 1: Criar o banco de dados
-    //PASSO 2: Usar a lib mysql2 para conectar com o banco
+import BancoMysql from './db/bancoMysql'
+
+app.get("/produtos",async(req,res)=>{
     try{
-        const conexao = await mysql.createConnection({
-            host: process.env.dbhost?process.env.dbhost:"localhost",
-            user:process.env.dbuser?process.env.dbuser:"root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
-            database:process.env.dbname?process.env.dbname:"banco1022b",
-            port:process.env.dbport?parseInt(process.env.dbport):3306
-        })
-        //PASSO 3: QUERY  -> SELECT * FROM produtos
-        const [result,fields]  = await conexao.query("SELECT * FROM produtos")
-        await conexao.end()
-        //PASSO 4: Colocar os dados do banco no response
+        const banco = new BancoMysql();
+        const result = banco.query()
+        banco.end()
         res.send(result)
     }catch(e){
         res.status(500).send("Erro do servidor")
