b19717fa1208350a53e83a188b61ea057953f6e7
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-01-15 08:00:29 -0400

    Atualização do GIT

---
+++ src/db/bancoMongo.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from 'mongodb';
+
+//const url = 'mongodb://localhost:27017';
+//const client = new MongoClient(url);
+//const dbName = 'banco1022b';
+//await client.connect();
+//const db = client.db(dbName);
+class BancoMongo{
+
+}
+
+export default BancoMongo
